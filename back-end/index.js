fe2c6b43d4d38270bf65c8e577e38c2e39b7f4b7
David Courtemanche <davidcourtemanche@hotmail.com>
2019-03-13 17:07:04 -0400

    back end upload working in postman, single image

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-let app = express();
+const multer = require('multer');
+const path = require ('path');
+const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors({
@@ -11,10 +13,62 @@ const PORT = 8080;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
-let profile = require('./routes/profile')
-app.use('/profile',profile)
+//storage and how file is named
+const storage = multer.diskStorage({
+  destination:'./public/uploads/',
+  filename: function (req,file,cb){
+  cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+
+//upload variable
+const upload = multer({
+  storage:storage,
+  limits:{
+      fileSize:100000000,
+      fileFilter:function(req,res,cb){
+          checkFileType(file,cb)
+      }
+  }
+}).single('myImage');
+
+
+//check file type function
+function checkFileType(file, cb){
+  //allowed file types
+  const fileTypes = /jpeg|jpg|png|gif|bmp|tif/;
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimetype)
+  if(mimetype && extName){
+      return cb(null, true);
+  } else {
+      cb('Error: images only')
+  }
+}
+
+//file.originalfilename is the original file name
+//file.filename is new file name
+app.post('/upload',(req,res) =>{
+  upload(req,res,(err) => {
+      if(err){
+        console.log(err)
+        res.send(err)
+      } else {
+          console.log(req.file)
+          if(req.file == undefined){
+            console.log("no file selected")
+            res.send("no file selected")
+          } else {
+            console.log("file uploaded as: " + req.file.filename)
+            res.send("file uploaded as: " + req.file.filename)
+          }
+      }
+  })
+})
+
+
 
 app.get('/getPics', (req,res) =>{
     let userFolder = 'tempImages'
@@ -43,9 +97,7 @@ app.get('/getPics', (req,res) =>{
         imgNumber++
       });
       res.json(arrayOfImages);
-    });
-
-    
+    });  
 })
 
 
